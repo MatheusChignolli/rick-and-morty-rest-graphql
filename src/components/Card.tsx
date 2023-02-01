0644d0d80ffb8112d203164ca2696a29426c7c5c
Matheus Chignolli <matheuschignolli@gmail.com>
2023-02-01 08:51:18 -0300

    create Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,67 @@
+import Ellipsis from './Ellipsis'
+
+export default ({ character, index }: { character: any; index: number }) => {
+  return (
+    <div
+      className={`w-full rounded-md p-4 flex gap-4 flex-col bg-gray-100  shadow-gray-900/40 shadow-inner`}
+    >
+      <div className="flex gap-4">
+        <img
+          className="rounded w-40 h-40 shadow-[0px_3px_6px_rgba(0,0,0,0.4),0px_8px_13px_-4px_rgba(0,0,0,0.3),0px_-3px_0px_inset_rgba(0,0,0,0.2)]"
+          src={character.image}
+          alt={`Image of ${character.name}`}
+        />
+        <ul className="w-full">
+          <li>
+            <div className="flex flex-col">
+              <label className="text-sm font-bold">Name</label>
+              <div className="text-md font-normal text-ellipsis">
+                <Ellipsis>{character.name}</Ellipsis>
+              </div>
+            </div>
+          </li>
+          <li className="mt-1">
+            <div className="flex flex-col">
+              <label className="text-sm font-bold">Specie</label>
+              <div className="text-md font-normal text-ellipsis">
+                <Ellipsis>{`${character.species} (${character.gender})`}</Ellipsis>
+              </div>
+            </div>
+          </li>
+          <li className="mt-1">
+            <div className="flex flex-col">
+              <label className="text-sm font-bold">Status</label>
+              <div className="text-md font-normal text-ellipsis">
+                <Ellipsis>{character.status}</Ellipsis>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <ul>
+        <li>
+          <div className="flex flex-col">
+            <label className="text-sm font-bold">Origin</label>
+            <div className="text-md font-normal text-ellipsis">
+              <Ellipsis>{`${character.origin.name}${
+                !!character.origin.dimension
+                  ? ` - ${character.origin.dimension}`
+                  : ''
+              }${
+                !!character.origin.type ? ` - ${character.origin.type}` : ''
+              }`}</Ellipsis>
+            </div>
+          </div>
+        </li>
+        <li className="mt-1">
+          <div className="flex flex-col">
+            <label className="text-sm font-bold">Location</label>
+            <div className="text-md font-normal text-ellipsis">
+              <Ellipsis>{character.location.name}</Ellipsis>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
--- src/pages/Graphql/index.tsx
+++ src/pages/Graphql/index.tsx
@@ -1,8 +1,8 @@
 import { memo, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
-import Ellipsis from '../../components/Ellipsis'
-import { GET_CHARACTERS } from '../../services/graphql/queries'
+import Card from '../../components/Card'
 import { PageInterface } from '../../types'
+import { GET_CHARACTERS } from './queries'
 
 interface getCharactersInterface {
   characters: {
@@ -10,85 +10,22 @@ interface getCharactersInterface {
   }
 }
 
-export default memo(({ name, status, gender }: PageInterface) => {
-  const { data, loading, refetch } = useQuery<getCharactersInterface>(GET_CHARACTERS)
+export default memo((filter: PageInterface) => {
+  const { data, loading, error, refetch } =
+    useQuery<getCharactersInterface>(GET_CHARACTERS)
 
   useEffect(() => {
-    refetch({ name, status, gender })
-  }, [name, status, gender])
+    refetch(filter)
+  }, [filter])
 
-  if (loading) {
-    return <div>Loading</div>
-  }
+  if (loading) return <div>Loading</div>
+  if (error) return <div>Error</div>
 
   return (
     <section className="h-full w-full grid grid-cols-4 gap-10">
       {data?.characters.results.map((character, index) => {
         return (
-          <div
-            key={`character-${index}`}
-            className={`w-full rounded-md p-4 flex gap-4 flex-col bg-gray-100  shadow-gray-900/40 shadow-inner`}
-          >
-            <div className="flex gap-4">
-              <img
-                className="rounded w-40 h-40 shadow-[0px_3px_6px_rgba(0,0,0,0.4),0px_8px_13px_-4px_rgba(0,0,0,0.3),0px_-3px_0px_inset_rgba(0,0,0,0.2)]"
-                src={character.image}
-                alt={`Image of ${character.name}`}
-              />
-              <ul className="w-full">
-                <li>
-                  <div className="flex flex-col">
-                    <label className="text-sm font-bold">Name</label>
-                    <div className="text-md font-normal text-ellipsis">
-                      <Ellipsis>{character.name}</Ellipsis>
-                    </div>
-                  </div>
-                </li>
-                <li className="mt-1">
-                  <div className="flex flex-col">
-                    <label className="text-sm font-bold">Specie</label>
-                    <div className="text-md font-normal text-ellipsis">
-                      <Ellipsis>
-                        {`${character.species} (${character.gender})`}
-                      </Ellipsis>
-                    </div>
-                  </div>
-                </li>
-                <li className="mt-1">
-                  <div className="flex flex-col">
-                    <label className="text-sm font-bold">Status</label>
-                    <div className="text-md font-normal text-ellipsis">
-                      <Ellipsis>{character.status}</Ellipsis>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-            <ul>
-              <li>
-                <div className="flex flex-col">
-                  <label className="text-sm font-bold">Origin</label>
-                  <div className="text-md font-normal text-ellipsis">
-                    <Ellipsis>{`${character.origin.name}${
-                      !!character.origin.dimension
-                        ? ` - ${character.origin.dimension}`
-                        : ''
-                    }${
-                      !!character.origin.type ? ` - ${character.origin.type}` : ''
-                    }`}</Ellipsis>
-                  </div>
-                </div>
-              </li>
-              <li className="mt-1">
-                <div className="flex flex-col">
-                  <label className="text-sm font-bold">Location</label>
-                  <div className="text-md font-normal text-ellipsis">
-                    <Ellipsis>{character.location.name}</Ellipsis>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
+          <Card key={`character-${index}`} character={character} index={index} />
         )
       })}
     </section>
